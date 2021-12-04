569823dc35bb5165ea67d0da731e4ed7f7c53d07
fabricio3g <fabricio.3g@gmail.com>
2021-12-04 00:53:11 -0300

    Put mapping added

--- src/main/java/com/example/demo/AddressController.java
+++ src/main/java/com/example/demo/AddressController.java
@@ -4,22 +4,32 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("addresses")
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
 
-    @GetMapping("/addresses")
+    
+    
+    @PostMapping
+    public Address updateAddress(@RequestBody Address address){
+        return addressService.create(address);
+    }
+
+    @GetMapping
     public List<Address> getAll(){
         return addressService.getAll();
     }
-    @PostMapping("/addresses")
-    public Address saveAddress(@RequestBody Address address){
-        return addressService.save(address);
+    @PutMapping
+    public Address createAddress(@RequestBody Address address){
+        return addressService.update(address);
     }
     
 }
